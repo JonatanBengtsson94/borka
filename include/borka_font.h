9c62b5d908a96821f7c7f833fe5819fc9b78a4a0
Jonatan <jonatan.bengtsson1994@gmail.com>
2025-12-16 23:33:57 +0100

    docs: added documentation

--- include/borka_font.h
+++ include/borka_font.h
@@ -4,10 +4,16 @@
 #include "borka_math.h"
 #include "borka_texture.h"
 
+/**
+ * @brief Bitmap font structure for text rendering
+ *
+ * @note All glyphs should be the same size (monospace font)
+ */
 typedef struct {
-  BrTexture *font_atlas;
-  BrVec2 glyph_size;
-  BrVec2 spacing;
+  BrTexture *font_atlas; /**< Texture atlas containing all glyphs. */
+  BrVec2 glyph_size;     /**< Size of each glyph in pixels (width, height). */
+  BrVec2 spacing;        /**< Space between glyphs when rendering (horizontal,
+                            vertical) in pixels. */
 } BrFont;
 
 #endif // BORKA_FONT_H
